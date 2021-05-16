298ed89e9320fee1d480a80bd085e4179d666bbd
Ban-Ironic-Ohms <micahrobinson93@gmail.com>
2021-05-15 22:26:15 -0700

    added linux file

---
+++ DiscordBot.py
@@ -0,0 +1,54 @@
+import discord
+from discord.ext import commands
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+        if message.content.startswith('\u203Dreset'):
+            if str(message.author) == "Micah#2740":
+                add_to = open("/home/pi/Desktop/DiscordBot/count.txt", "w")
+                add_to.write("0")
+                add_to.close()
+                await message.reply("And from the sky, the number hath been stricken back to 0")
+            else:
+                await message.reply("I'm afraid I can't do that, Dave")
+
+        elif message.content.startswith('\u203Doff'):
+            await message.reply("Wrong")
+
+        elif message.content.startswith('\u203D'):
+            count = open("/home/pi/Desktop/DiscordBot/count.txt", "r")
+            try:
+            	current_count = int(count.read())
+            except ValueError:
+                current_count = 0
+            count.close()
+
+            user_content = message.content[1:]
+
+            try:
+                user_content = int(user_content)
+            except TypeError:
+                return
+
+
+            current_count += user_content
+
+            add_to = open("/home/pi/Desktop/DiscordBot/count.txt", "w")
+            add_to.write(str(current_count))
+
+            await message.reply("The total dislike of the new Discord is {}".format(current_count), mention_author=True)
+            print(message.content)
+
+client = MyClient()
+
+keyfile = open("/home/pi/Desktop/DiscordBot/key.txt", "r")
+
+logon_key = str(keyfile.read())
+
+client.run(logon_key)
