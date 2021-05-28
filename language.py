179b713a7b87823f6695b9186ddc2fbdac87ec7a
Micah Robinson <micahrobinson93@gmail.com>
2021-05-27 20:37:59 -0700

    added language checker

---
+++ language.py
@@ -0,0 +1,8 @@
+import language_check
+
+tool = language_check.LanguageTool('en-US')
+
+def CheckText(message):
+    matches = tool.check(message)
+    for mistake in matches:
+        return mistake
