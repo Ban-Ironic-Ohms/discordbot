357e3bff9eec44df8ed72f66267271c0f59c83be
Micah Robinson <micahrobinson93@gmail.com>
2021-05-27 22:35:07 -0700

    fixed life

--- DiscordBot.py
+++ DiscordBot.py
@@ -8,8 +8,9 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
-
-        await message.reply(CheckText(str(message.content)))
+        
+        if str(message.author != "Micah's Marvelous Discord Bot#3557"):
+            await message.reply(CheckText(str(message.content)))
 
         if message.content.startswith('\u203Dreset'):
             if str(message.author) == "Micah#2740":
