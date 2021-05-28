c8f05d3b8bc1c4e805efff6fc84db2325abc4ec9
Micah Robinson <micahrobinson93@gmail.com>
2021-05-27 22:53:51 -0700

    fixed life

--- DiscordBot.py
+++ DiscordBot.py
@@ -11,8 +11,9 @@ class MyClient(discord.Client):
         
         if not message.content.startswith("Line"):
             n_o_errors = numberOfErrors(str(message.content))
-            ret_message = "There are", n_o_errors, "grammatical or spelling errors in your message."
-            await message.reply(ret_message)
+            ret_message = 'There are' n_o_errors 'grammatical or spelling errors in your message'
+            if n_o_errors != 0:
+                await message.reply(ret_message)
             #await message.reply(checkText(str(message.content)))
 
         if message.content.startswith('\u203Dreset'):
